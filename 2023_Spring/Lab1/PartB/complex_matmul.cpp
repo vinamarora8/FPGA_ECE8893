eb4deee301a6112be8f3b3837f466823fb7b5514
Vinam Arora <vinam@posteo.de>
2023-02-03 20:11:47 -0500

    Adds intial complex mult

--- 2023_Spring/Lab1/PartB/complex_matmul.cpp
+++ 2023_Spring/Lab1/PartB/complex_matmul.cpp
@@ -10,8 +10,8 @@
 #include "complex.h"
 
 void complex_matmul(
-    complex_t MatA_DRAM[M][N], 
-    complex_t MatB_DRAM[N][K], 
+    complex_t MatA_DRAM[M][N],
+    complex_t MatB_DRAM[N][K],
     complex_t MatC_DRAM[M][K]
 )
 {
@@ -22,6 +22,65 @@ void complex_matmul(
 #pragma HLS interface s_axilite port=return
 
     // TODO: Insert your code here
-    // You may follow a similar style as in real_matmul.cpp    
+    // You may follow a similar style as in real_matmul.cpp
+
+    complex_t MatA[M][N];
+    complex_t MatB[N][K];
+    complex_t MatC[M][K];
+
+    // Read in the data (Matrix A) from DRAM to BRAM
+    MAT_A_ROWS:
+    for(int i = 0; i < M; i++) {
+        MAT_A_COLS:
+        for(int j = 0; j < N; j++) {
+            MatA[i][j] = MatA_DRAM[i][j];
+        }
+    }
+
+    // Read in the data (Matrix B) from DRAM to BRAM
+    MAT_B_ROWS:
+    for(int i = 0; i < N; i++) {
+        MAT_B_COLS:
+        for(int j = 0; j < K; j++) {
+            MatB[i][j] = MatB_DRAM[i][j];
+        }
+    }
+
+    // Initialize product matrix C
+    MAT_C_ROWS_INIT:
+    for(int i = 0; i < M; i++) {
+        MAT_C_COLS_INIT:
+        for(int j = 0; j < K; j++) {
+            MatC[i][j] = {0, 0};
+        }
+    }
+
+    // Perform matrix multiplication
+    OUTER_ROWS:
+    for(int i = 0; i < M; i++) {
+        OUTER_COLS:
+        for(int j = 0; j < K; j++) {
+            INNER_ROW_COL:
+            complex_t cijLocal = {0, 0};
+            for(int p = 0; p < N; p++) {
+                cijLocal.real +=   MatA[i][p].real * MatB[p][j].real;
+                cijLocal.real += - MatA[i][p].imag * MatB[p][j].imag;
+
+                cijLocal.imag +=   MatA[i][p].real * MatB[p][j].imag;
+                cijLocal.imag +=   MatA[i][p].imag * MatB[p][j].real;
+            }
+            MatC[i][j] = cijLocal;
+
+        }
+    }
+
+    // Write back the data from BRAM to DRAM
+    MAT_C_ROWS:
+    for(int i = 0; i < M; i++) {
+        MAT_C_COLS:
+        for(int j = 0; j < K; j++) {
+            MatC_DRAM[i][j] = MatC[i][j];
+        }
+    }
 
 }
