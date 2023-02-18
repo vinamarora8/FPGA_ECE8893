8d44b8351200bd439f6e3427244b938022f82ef4
Vinam Arora <vinam@posteo.de>
2023-02-18 14:10:45 -0500

    Adds logic to model_conv.cpp

--- 2023_Spring/Lab2/PartA/model_conv.cpp
+++ 2023_Spring/Lab2/PartA/model_conv.cpp
@@ -9,6 +9,17 @@
 
 #include "conv.h"
 
+#define KERN_HEIGHT 7
+#define KERN_WIDTH 7
+
+static float relu(float x)
+{
+    if (x < 0)
+      return 0.0;
+    else
+      return x;
+}
+
 void model_conv (
     fm_t input_feature_map[3][736][1280],
     wt_t layer_weights[64][3][7][7],
@@ -17,7 +28,7 @@ void model_conv (
 )
 {
 //--------------------------------------------------------------------------
-// Your code for TASK A goes here 
+// Your code for TASK A goes here
 //
 // Implement the 7x7 convolution layer in typical C/C++ manner.
 // You are free to use any C/C++ programming constructs which may
@@ -25,8 +36,40 @@ void model_conv (
 //
 // The sole purpose of this code is to get the functionality right!
 //
-// Hints: 
+// Hints:
 // - Handle stride and border pixels appropriately.
 // - Do not forget to add bias and apply ReLU!
 //--------------------------------------------------------------------------
+
+    for (int of = 0; of < OUT_FM_DEPTH; of++)
+      for (int oh = 0; oh < OUT_FM_HEIGHT; oh++)
+        for (int ow = 0; ow < OUT_FM_WIDTH; ow++)
+        {
+
+            output_feature_map[of][oh][ow] = 0;
+
+            float lin_op = 0;
+
+            for (int id = 0; id < IN_FM_DEPTH; id++)
+              for (int kh = 0; kh < KERN_HEIGHT; kh++)
+                for (int kw = 0; kw < KERN_WIDTH; kw++)
+                {
+
+                    int idx_h = (STRIDE * oh) - PADDING + kh;
+                    int idx_w = (STRIDE * ow) - PADDING + kw;
+
+                    if (idx_h < 0 || idx_w < 0)
+                      continue;
+
+                    if (idx_h >= IN_FM_HEIGHT || idx_w >= IN_FM_WIDTH)
+                      continue;
+
+                    lin_op += input_feature_map[id][idx_h][idx_w] * layer_weights[of][id][kh][kw];
+
+                }
+
+
+            output_feature_map[of][oh][ow] += relu(lin_op + layer_bias[of]);
+        }
+
 }
